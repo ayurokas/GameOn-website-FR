85092714b02b35d3d65540612ad4926ed8273371
saihyura <elisa.github@gmail.com>
2023-03-16 12:55:34 +0100

    ajout du js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,8 +9,15 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const closeButton = document.getElementsByClassName("close")[0];
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+
+
+// Bouton qui ferme l'inscription 
+closeButton.onclick = function() {
+  modalbg.style.display = "none";
+}
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
