0506b57b7a6fe28947330177d5eb84177541f3a0
saihyura <elisa.github@gmail.com>
2023-03-22 11:28:50 +0100

    ajout commentaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,90 +8,106 @@ function editNav() {
 }
 
 // DOM Elements
-// Sélection des éléments du DOM
-const modalbg = document.querySelector(".bground");
-const form = document.querySelector("#form");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const confirmationMessage = document.querySelector(".formok");
-const confirmationButton = document.querySelector(".formok-close");
-const firstName = document.querySelector("#firstname");
-const lastName = document.querySelector("#lastname");
-const email = document.querySelector("#email");
-const birthdate = document.querySelector("#birthdate");
-const quantity = document.querySelector("#quantity");
-const locationList = document.querySelectorAll(".locationlist input[type=radio]");
-const tosCheckbox = document.querySelector("#tos");
-const newsCheckbox = document.querySelector("#news");
-const closeButton = document.querySelector(".btn-close");
+// "DOM Elements" est utilisée pour sélectionner et stocker des éléments HTML spécifiques dans des variables
+// Cela permet d'accéder facilement aux éléments du formulaire HTML à partir du code JavaScript.
+const modalbg = document.querySelector(".bground"); //la boîte de dialogue modale affichée lorsque l'utilisateur clique sur le bouton "Je m'inscris" ou "Inscription".
+const form = document.querySelector("#form");//formulaire dans la boîte de dialogue modale.
+const modalBtn = document.querySelectorAll(".modal-btn");//boutons qui ouvrent la boîte de dialogue modale.
+const formData = document.querySelectorAll(".formData");//les champs du formulaire.
+const confirmationMessage = document.querySelector(".formok");//message de confirmation affiché lorsque le formulaire est soumis avec succès.
+const confirmationButton = document.querySelector(".formok-close");//le bouton "Fermer" dans le message de confirmation.
+const firstName = document.querySelector("#firstname");//le champ du formulaire pour le prénom.
+const lastName = document.querySelector("#lastname");//le champ du formulaire pour le nom.
+const email = document.querySelector("#email");//le champ du formulaire pour le mail.
+const birthdate = document.querySelector("#birthdate");//le champ du formulaire pour la date d'anniversaire.
+const quantity = document.querySelector("#quantity");//formulaire pour le nombre de concours.
+const locationList = document.querySelectorAll(".locationlist input[type=radio]");//les boutons radio pour la sélection de la ville.
+const tosCheckbox = document.querySelector("#tos");//case à cocher pour l'acceptation des conditions générales.
+const newsCheckbox = document.querySelector("#news");//la case à cocher pour la réception de la newsletter.
+const closeButton = document.querySelector(".btn-close");// le bouton "Fermer" dans la boîte de dialogue modale.
 
-
-// Fonction de validation du formulaire
+//fonction "validate()" est utilisée pour valider les données saisies par l'utilisateur dans le formulaire avant de le soumettre
+//parcourt chaque champ du formulaire, vérifie si les données saisies sont valides 
+//met à jour les messages d'erreur et les styles CSS correspondants en conséquence.
 function validate() {
-  let isValid = true;
+  let isValid = true; //variable stocke l'état de la validation du formulaire, qui est initialisée à "true" car le formulaire n'a pas encore été validé.
 
-  // Validation du prénom
-  if (firstName.value.trim().length < 2) {
-    document.querySelector("#firstname-error").innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-    isValid = false;
-  } else {
-    document.querySelector("#firstname-error").innerHTML = "";
-  }
+  // varifie si le champ du prénom est valide 
+  //Si la longueur de la valeur du champ du prénom est inférieure à 2, 
+  //cela signifie que le champ est vide ou que l'utilisateur a entré moins de 2 caractères. 
+if (firstName.value.trim().length < 2) {
+  document.querySelector("#firstname-error").innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+  isValid = false;
+  firstName.classList.add("error-input");
+} else {
+  document.querySelector("#firstname-error").innerHTML = "";
+  firstName.classList.remove("error-input");
+}
 
-  // Validation du nom de famille
-  if (lastName.value.trim().length < 2) {
-    document.querySelector("#lastname-error").innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-    isValid = false;
-  } else {
-    document.querySelector("#lastname-error").innerHTML = "";
-  }
+// Validation du nom de famille
+if (lastName.value.trim().length < 2) {
+  document.querySelector("#lastname-error").innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+  isValid = false;
+  lastName.classList.add("error-input");
+} else {
+  document.querySelector("#lastname-error").innerHTML = "";
+  lastName.classList.remove("error-input");
+}
 
-  // Validation de l'adresse électronique
-  if (!emailIsValid(email.value)) {
-    document.querySelector("#email-error").innerHTML = "Veuillez entrer une adresse email valide.";
-    isValid = false;
-  } else {
-    document.querySelector("#email-error").innerHTML = "";
-  }
+// Validation de l'adresse électronique
+if (!emailIsValid(email.value)) {
+  document.querySelector("#email-error").innerHTML = "Veuillez entrer une adresse email valide.";
+  isValid = false;
+  email.classList.add("error-input");
+} else {
+  document.querySelector("#email-error").innerHTML = "";
+  email.classList.remove("error-input");
+}
 
-  // Validation de la date de naissance
-  if (!birthdate.value) {
-    document.querySelector("#birthdate-error").innerHTML = "Vous devez entrer votre date de naissance.";
-    isValid = false;
-  } else {
-    document.querySelector("#birthdate-error").innerHTML = "";
-  }
+// Validation de la date de naissance
+if (!birthdate.value) {
+  document.querySelector("#birthdate-error").innerHTML = "Vous devez entrer votre date de naissance.";
+  isValid = false;
+  birthdate.classList.add("error-input");
+} else {
+  document.querySelector("#birthdate-error").innerHTML = "";
+  birthdate.classList.remove("error-input");
+}
 
-  // Validation du nombre de concours
-  if (isNaN(quantity.value) || quantity.value < 0) {
-    document.querySelector("#quantity-error").innerHTML = "Vous devez entrer une valeur numérique pour le nombre de concours.";
-    isValid = false;
-  } else {
-    document.querySelector("#quantity-error").innerHTML = "";
-  }
+// Validation du nombre de concours
+if (isNaN(quantity.value) || quantity.value < 0) {
+  document.querySelector("#quantity-error").innerHTML = "Vous devez entrer une valeur numérique pour le nombre de concours.";
+  isValid = false;
+  quantity.classList.add("error-input");
+} else {
+  document.querySelector("#quantity-error").innerHTML = "";
+  quantity.classList.remove("error-input");
+}
 
-  // Validation de la sélection de la ville
-  let locationChecked = false;
-  for (let i = 0; i < locationList.length; i++) {
-    if (locationList[i].checked) {
-      locationChecked = true;
-      break;
-    }
+// Validation de la sélection de la ville
+let locationChecked = false;
+for (let i = 0; i < locationList.length; i++) {
+  if (locationList[i].checked) {
+    locationChecked = true;
+    break;
   }
-  if (!locationChecked) {
-    document.querySelector("#location-error").innerHTML = "Vous devez choisir une option.";
-    isValid = false;
-  } else {
-    document.querySelector("#location-error").innerHTML = "";
-  }
+}
+if (!locationChecked) {
+  document.querySelector("#location-error").innerHTML = "Vous devez choisir une option.";
+  isValid = false;
+} else {
+  document.querySelector("#location-error").innerHTML = "";
+}
 
-  // Validation de l'acceptation des conditions générales
-  if (!tosCheckbox.checked) {
-    document.querySelector(".errormsg").innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
-    isValid = false;
-  } else {
-    document.querySelector(".errormsg").innerHTML = "";
-  }
+// Validation de l'acceptation des conditions générales
+if (!tosCheckbox.checked) {
+  document.querySelector(".errormsg").innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
+  isValid = false;
+  tosCheckbox.classList.add("error-input");
+} else {
+  document.querySelector(".errormsg").innerHTML = "";
+  tosCheckbox.classList.remove("error-input");
+}
 
   return isValid;
 }
